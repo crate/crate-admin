299eaf312f468bd372b372633df061b001a3040a
Yiorgis Gozadinos <ggozad@crypho.com>
2014-02-19 16:44:48 +0100

    Render the StatusBarTemplate

--- app/js/statusbar.js
+++ app/js/statusbar.js
@@ -10,24 +10,33 @@ define(['jquery',
         StatusBarView: Backbone.View.extend({
 
             el: '#statusbar',
+            template: _.template(StatusBarTemplate),
 
             events: {
             },
 
+            data: {},
+
             initialize: function () {
+                this.update();
             },
 
-            data: function () {
-                return {};
+            update: function () {
+                var self = this;
+                $.get('/_nodes/stats?all=true')
+                    .done(function (data) {
+                        self.data = data;
+                        self.render();
+                    });
+                setTimeout(function () { self.update(); }, 1000);
             },
 
             render: function () {
-                this.$el.html(this.template(this.data()));
+                this.$el.html(this.template(this.data));
                 return this;
             }
         })
     };
-
     StatusBar.StatusBarView.prototype.template = _.template(StatusBarTemplate);
 
     return StatusBar;
