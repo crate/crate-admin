b5dd5132b04093c104dd5da6432e447fb05c6028
Yiorgis Gozadinos <ggozad@crypho.com>
2014-02-19 17:48:06 +0100

    Calculate and render load on status bar

--- app/js/statusbar.js
+++ app/js/statusbar.js
@@ -1,13 +1,14 @@
 define(['jquery',
         'underscore',
         'backbone',
+        'base',
         'text!views/statusbar.html',
         'bootstrap'
-    ], function ($, _, Backbone, StatusBarTemplate) {
+    ], function ($, _, Backbone, base, StatusBarTemplate) {
 
     var StatusBar = {
 
-        StatusBarView: Backbone.View.extend({
+        StatusBarView: base.CrateView.extend({
 
             el: '#statusbar',
             template: _.template(StatusBarTemplate),
@@ -15,29 +16,59 @@ define(['jquery',
             events: {
             },
 
-            data: {},
+            default_data: {
+                cluster_name: '',
+                cluster_state: '',
+                cluster_color_label: 'label-default',
+                load: ['-.-', '-.-', '-.-']
+            },
 
             initialize: function () {
                 this.update();
             },
 
+            clusterLoad: function (nodes) {
+                var nodes_count = 0;
+                var load = [0.0, 0.0, 0.0];
+                for (var node in nodes) {
+                    nodes_count++;
+                    for (var i=0; i<3; i++) {
+                        load[i] = load[i]+nodes[node].os.load_average[i];
+                    }
+                }
+                for (var i=0; i<3; i++) {
+                    load[i] = load[i]/nodes_count;
+                    load[i] = load[i].toFixed(2);
+                }
+                return load;
+            },
+
             update: function () {
-                var self = this;
+                var self = this,
+                    load;
                 $.get('/_nodes/stats?all=true')
                     .done(function (data) {
-                        self.data = data;
+                        self.data = {
+                            cluster_name: data.cluster_name,
+                            load: self.clusterLoad(data.nodes),
+                            cluster_color_label: '',
+                            cluster_state: ''
+                        };
                         self.render();
+                    })
+                    .error(function() {
+                        delete self.data;
                     });
+
                 setTimeout(function () { self.update(); }, 1000);
             },
 
             render: function () {
-                this.$el.html(this.template(this.data));
+                this.$el.html(this.template(this.data || this.default_data));
                 return this;
             }
         })
     };
-    StatusBar.StatusBarView.prototype.template = _.template(StatusBarTemplate);
 
     return StatusBar;
 });
