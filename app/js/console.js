2858ae298d009187dfe3740a95ad2e229d7150ff
Yiorgis Gozadinos <ggozad@crypho.com>
2014-02-20 15:06:49 +0100

    Execute query on form submission

--- app/js/console.js
+++ app/js/console.js
@@ -2,9 +2,10 @@ define(['jquery',
         'underscore',
         'backbone',
         'base',
+        'SQL',
         'text!views/console.html',
         'bootstrap'
-    ], function ($, _, Backbone, base, ConsoleTemplate) {
+    ], function ($, _, Backbone, base, SQL, ConsoleTemplate) {
 
     var Console = {};
 
@@ -13,12 +14,28 @@ define(['jquery',
         id: 'page-wrapper',
         template: _.template(ConsoleTemplate),
 
-        initialize: function () {
-            this.listenTo(this.model, 'change', this.render);
+        events: {
+            'submit form': 'execute'
+        },
+
+        execute: function (ev) {
+
+            var sq,
+                self = this,
+                stmt = this.$('#query').val();
+
+            ev.preventDefault();
+            ev.stopPropagation();
+            this.$('.alert').hide();
+            sq = new SQL.Query(stmt);
+            sq.execute().done(function (res) {
+            }).error(function (err) {
+            });
         },
 
         render: function () {
             this.$el.html(this.template(this.model.toJSON()));
+            this.$('.alert').hide();
             return this;
         }
     });
