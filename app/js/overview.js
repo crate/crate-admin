8600310add922f6828cce785b010f86fd2c43cb0
Yiorgis Gozadinos <ggozad@crypho.com>
2014-02-28 12:26:43 +0100

    remove comment

--- app/js/overview.js
+++ app/js/overview.js
@@ -15,7 +15,6 @@ define(['jquery',
         template: _.template(OverviewTemplate),
 
         initialize: function () {
-            // this.listenTo(this.model, 'change:loadHistory', this.updateLoadGraph);
             this.listenTo(this.model, 'change', this.render);
         },
 
