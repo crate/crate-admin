02fb889649cf88ec3d11c258ec2482cdadf34141
Christian Haudum <christian.haudum@gmail.com>
2015-06-11 16:49:37 +0200

    made overview module more Angular-esque

--- app/scripts/controllers/overview.js
+++ app/scripts/controllers/overview.js
@@ -1,8 +1,50 @@
 'use strict';
 
 angular.module('overview', ['stats'])
-  .controller('OverviewController', function ($scope, $location, $log, $timeout, ClusterState) {
-
+  .factory('ZeroArray', function() {
+    return function ZeroArray(len) {
+      var res = new Array(len);
+      for (var i=0; i<res.length; i++) {
+        res[i] = 0;
+      }
+      return res;
+    };
+  })
+  .factory('HistoryChart', function() {
+    return function HistoryChart(size){
+      var _data = [{
+          "key": "Load 1",
+          "values": [],
+          "color": "#55d4f5",
+          "area": true
+        }, {
+          "key": "Load 5",
+          "values": [],
+          "color": "#5987ff"
+        }, {
+          "key": "Load 15",
+          "values": [],
+          "color": "#115097"
+        }];
+      this.size = size;
+      this.data = function(){
+        return _data;
+      };
+      this.update = function(data){
+        var graphData = [[], [], []];
+        for (var i=0; i<this.size; i++) {
+          graphData[0][i] = [i, data[0][i]];
+          graphData[1][i] = [i, data[1][i]];
+          graphData[2][i] = [i, data[2][i]];
+        }
+        for (var i=0; i<graphData.length; i++) {
+          _data[i].values = graphData[i];
+        }
+        return this;
+      };
+    };
+  })
+  .controller('OverviewController', function($scope, $location, $log, $timeout, ClusterState, HistoryChart, ZeroArray) {
     var lastUpdate = null;
     var colorMap = {
       "good": 'panel-success',
@@ -20,7 +62,6 @@ angular.module('overview', ['stats'])
     $scope.cluster_color_class = 'panel-default';
     $scope.chart = {
       'data': [],
-      'maxY': 1.0
     };
 
     $scope.$watch(function() { return ClusterState.data; }, function (data) {
@@ -69,71 +110,24 @@ angular.module('overview', ['stats'])
       }
     }, true);
 
-    function fillArrayWithZeroes(len) {
-      var res = new Array(len);
-      for (var i=0; i<res.length; i++) {
-        res[i] = 0;
-      }
-      return res;
-    };
+    // create chart instance that holds meta information and data
+    var chart = new HistoryChart(ClusterState.HISTORY_LENGTH);
 
     var drawGraph = function drawGraph(history) {
       var len = history[0].length;
-
       // Create Arrays with Zeroes in it
       var data = [
-        fillArrayWithZeroes(ClusterState.HISTORY_LENGTH-len),
-        fillArrayWithZeroes(ClusterState.HISTORY_LENGTH-len),
-        fillArrayWithZeroes(ClusterState.HISTORY_LENGTH-len)
+        ZeroArray(ClusterState.HISTORY_LENGTH-len),
+        ZeroArray(ClusterState.HISTORY_LENGTH-len),
+        ZeroArray(ClusterState.HISTORY_LENGTH-len)
       ];
-
       for (var i=0; i<data.length; i++){
         data[i].push.apply(data[i], history[i]);
       }
-
-      var labels = new Array(ClusterState.HISTORY_LENGTH);
-      for (var i=0; i<labels.length; i++) {
-        labels[i] = i;
-      }
-
-      // Get the max value out of the history
-      var maxY = Math.max(
-        Math.max.apply(Math, data[0]),
-        Math.max.apply(Math, data[1]),
-        Math.max.apply(Math, data[2])
-      );
-
-      var graphData = [[], [], []];
-
-      for (var i=0; i<ClusterState.HISTORY_LENGTH; i++) {
-        graphData[0][i] = [labels[i], data[0][i]];
-        graphData[1][i] = [labels[i], data[1][i]];
-        graphData[2][i] = [labels[i], data[2][i]];
-      }
-
-      var chart = d3.select('#history-chart svg');
-
-      $scope.chart.y = [0, Math.round(Math.max(maxY+0.2, 1.0)*10)/10];
-      $scope.chart.data = [
-      {
-        "key": "Load 1",
-        "values": graphData[0],
-        "color": "#55d4f5",
-        "area": true
-      },
-      {
-        "key": "Load 5",
-        "values": graphData[1],
-        "color": "#5987ff"
-      },
-      {
-        "key": "Load 15",
-        "values": graphData[2],
-        "color": "#115097"
-      }];
+      $scope.chart.data = chart.update(data).data();
     };
 
     // bind tooltips
     $("[rel=tooltip]").tooltip({ placement: 'top'});
 
-  });
+  })
