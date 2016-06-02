04ebbef052db1bb18e43699aae79458aac073b51
Christian Haudum <christian.haudum@gmail.com>
2016-06-02 14:42:13 +0200

    converted clustercheck.js to ES2015

--- app/scripts/services/clustercheck.js
+++ app/scripts/services/clustercheck.js
@@ -2,30 +2,25 @@
 
 angular.module('clustercheck', [])
   .factory('ClusterCheck', ['SQLQuery', 'queryResultToObjects', '$q',
-    function (SQLQuery, queryResultToObjects, $q) {
-      var self = {
-        deferred: $q.defer()
+    function(SQLQuery, queryResultToObjects, $q) {
+
+      const cols = ['id', 'severity', 'description', 'passed'];
+      const stmt = 'SELECT id, severity, description, passed FROM sys.checks WHERE passed = false ORDER BY severity DESC, id';
+
+      return {
+        deferred: $q.defer(),
+        executeStmt: () => {
+          let deferred = $q.defer();
+          let promise = deferred.promise;
+
+          SQLQuery.execute(stmt)
+            .success((query) => {
+              var result = queryResultToObjects(query, cols);
+              deferred.resolve(result);
+            })
+            .error(deferred.reject);
+          return promise;
+        }
       };
-
-      var stmt = 'SELECT id, severity, description, passed FROM sys.checks WHERE passed = false ORDER BY severity DESC, id';
-
-      self.executeStmt = function() {
-        var deferred = $q.defer(),
-            promise = deferred.promise;
-
-        SQLQuery.execute(stmt)
-          .success(function(query) {
-            var result = queryResultToObjects(query,
-                ['id', 'severity', 'description', 'passed']);
-            deferred.resolve(result);
-          })
-          .error(function() {
-            deferred.reject();
-          });
-
-        return promise;
-      };
-
-      return self;
     }
   ]);
