a3fbfcc4eeae0095c54f5b2f24f3ca3c60aa2dd6
Christian Haudum <christian.haudum@gmail.com>
2016-06-02 15:11:37 +0200

    converted health.js to ES2015

--- app/scripts/services/health.js
+++ app/scripts/services/health.js
@@ -1,32 +1,27 @@
 'use strict';
 
 angular.module('health', [])
-  .factory('Health', function(){
-    var map = {};
-    var Health = function Health(level){
-      this.set = function set(level) {
-        this.level = isNaN(level) ? Health.UNKNOWN : level;
-        this.name = Health.NAMES[this.level] || '--';
-      };
-      this.toString = function(){
+  .factory('Health', [function(){
+    const NAMES = ['--', 'good', 'warning', 'critical']
+
+    class Health {
+      constructor(level) {
+        this.setLevel(level);
+      }
+      setLevel(level) {
+        this.level = this.isValid(level) ? level : -1;
+        this.name = NAMES[this.level + 1];
+      }
+      isValid(level) {
+        return !isNaN(level) && level >= -1 && level < NAMES.length;
+      }
+      toString() {
         return this.name;
-      };
-      this.set(level);
-    };
-
-    // human readable names
-    Health.NAMES = ['good', 'warning', 'critical'];
-    // create reverse map
-    Health.NAMES.map(function(obj, idx){ map[obj] = idx; });
-
-    Health.UNKNOWN = -1;
-    Health.GOOD = 0;
-    Health.WARNING = 1;
-    Health.CRITICAL = 2;
-
-    Health.fromString = function(name){
-      return new Health(map[name]);
-    };
+      }
+      static levelFromString(name) {
+        return NAMES.indexOf(name) - 1;
+      }
+    }
 
     return Health;
-  });
+  }]);
