d84f8ec206360d125c16e4424e8d627bf07b5611
Christian Haudum <christian.haudum@gmail.com>
2016-06-02 09:12:40 +0200

    converted text.js to ES2015

--- app/scripts/filter/text.js
+++ app/scripts/filter/text.js
@@ -2,7 +2,7 @@
 
 angular.module('common')
   .filter('capitalize', function() {
-    return function(input, scope) {
-        return input.substring(0,1).toUpperCase()+input.substring(1);
+    return (input, scope) => {
+        return input.substring(0, 1).toUpperCase() + input.substring(1);
     };
   });
