80faf014f7776cc0df925a70135527bec8a88a26
Christian Haudum <christian.haudum@gmail.com>
2016-06-02 09:11:24 +0200

    converted numbers.js to ES2015

--- app/scripts/filter/numbers.js
+++ app/scripts/filter/numbers.js
@@ -1,18 +1,16 @@
 'use strict';
 
 angular.module('common')
-  .filter('floor', function($filter){
-    return function(value, fraction) {
-      var n = Math.pow(10, fraction);
+  .filter('floor', function($filter) {
+    return (value, fraction) => {
+      let n = Math.pow(10, fraction);
       return $filter('number')(Math.floor(value*n)/n, fraction);
     };
   })
   .filter('roundWithUnit', function($filter, $sce) {
-    return function(input, fraction) {
-      if (fraction == undefined) {
-        fraction = 3;
-      }
-      var res = '';
+    return (input, fraction) => {
+      if (typeof fraction === 'undefined') fraction = 3;
+      let res = '';
       if (Math.abs(Number(input)) >= 1.0e+9) {
         res = $filter('number')(Math.abs(Number(input)) / 1.0e+9, fraction) + "\u00A0Billion";
       } else if ( Math.abs(Number(input)) >= 1.0e+6) {
@@ -24,13 +22,13 @@ angular.module('common')
     };
   })
   .filter('bytes', function($sce) {
-        return function(bytes, precision) {
-                if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
-                if (bytes === 0) return '0\u00A0B'
-                if (typeof precision === 'undefined') precision = 1;
-                var units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'],
-                        number = Math.floor(Math.log(bytes) / Math.log(1024));
-                var res = (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  '\u00A0' + units[number];
-                return $sce.trustAsHtml(res);
-        };
-   });
+    const units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'];
+    return (bytes, precision) => {
+      if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
+      if (bytes === 0) return '0\u00A0B'
+      if (typeof precision === 'undefined') precision = 1;
+      let number = Math.floor(Math.log(bytes) / Math.log(1024));
+      let res = (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  '\u00A0' + units[number];
+      return $sce.trustAsHtml(res);
+    };
+  });
