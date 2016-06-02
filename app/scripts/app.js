00e0a00272f7757f339402c2dfec4ae3846a7c50
Christian Haudum <christian.haudum@gmail.com>
2016-06-02 08:53:10 +0200

    updated app.js to ES2015

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -52,7 +52,7 @@
 
   var head = document.getElementsByTagName('head')[0];
 
-  var loadScript = function loadScript(url) {
+  var loadScript = (url) => {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, false);
     xhr.send('');
@@ -63,10 +63,10 @@
   };
 
   // todo: load json from rest endpoint
-  $.get("conf/plugins.json", function(plugins){
+  $.get("conf/plugins.json", (plugins) => {
 
     console.info("Loaded Modules:", MODULES);
-    console.info("Loaded Plugins:", plugins.map(function(o){ return o.name; }));
+    console.info("Loaded Plugins:", plugins.map(function(o) { return o.name; }));
 
     for (var i=0; i<plugins.length; i++) {
       var module = plugins[i];
@@ -77,7 +77,7 @@
     var app = angular.module('crate', MODULES);
 
     app.config(['$routeProvider', '$httpProvider',
-      function ($routeProvider, $httpProvider) {
+      ($routeProvider, $httpProvider) => {
         // Enabling CORS in Angular JS
         $httpProvider.defaults.useXDomain = true;
         // register default routing
@@ -132,9 +132,7 @@
       }
     });
 
-    angular.element(document).ready(function() {
-      angular.bootstrap(document, ['crate']);
-    });
+    angular.element(document).ready(() => angular.bootstrap(document, ['crate']));
 
   });
 
