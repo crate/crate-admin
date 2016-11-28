12e26acd007f6e97955e4b2315bde45fff96bb07
meriamk <meriam.kharbat@gmail.com>
2016-11-28 14:35:22 +0100

    add tests for health.js

---
+++ app/tests/services/health.tests.js
@@ -0,0 +1,49 @@
+describe('Health', function() {
+  'use strict';
+
+  var mockHealthService;
+
+  beforeEach(module('common'));
+  beforeEach(module('crate'));
+  beforeEach(module('health'));
+  beforeEach(function() {
+    angular.mock.inject(function($injector) {
+      mockHealthService = $injector.get('Health');
+    });
+  });
+
+  describe('mockHealthService', function() {
+
+    it('should return 0', inject(function() {
+      var health = mockHealthService.fromString('good');
+      expect(health.name).toEqual("good");
+      expect(health.level).toEqual(0);
+
+    }));
+
+
+    it('should return 1', inject(function() {
+      var health = mockHealthService.fromString('warning');
+      expect(health.name).toEqual("warning");
+      expect(health.level).toEqual(1);
+
+    }));
+
+
+    it('should return 2', inject(function() {
+      var health = mockHealthService.fromString('critical');
+      expect(health.name).toEqual("critical");
+      expect(health.level).toEqual(2);
+
+    }));
+
+    it('should return 2', inject(function() {
+      var health = mockHealthService.fromString('something');
+      expect(health.name).toEqual("--");
+      expect(health.level).toEqual(-1);
+
+    }));
+
+  });
+
+});
