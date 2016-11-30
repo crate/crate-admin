d37391ed2f5e20392f40b00ab0b424d8d9eb9ee4
meriamk <meriam.kharbat@gmail.com>
2016-11-30 15:47:51 +0100

    fix up! complete coverage for filters

--- app/tests/filter/text.test.js
+++ app/tests/filter/text.test.js
@@ -54,6 +54,12 @@ describe('severityClass', function() {
 		severityClass = $filter('severityClass');
 		expect(severityClass(3)).toBe("severity--danger");
 	});
+
+	it('should return severity-info', function() {
+		var severityClass;
+		severityClass = $filter('severityClass');
+		expect(severityClass(0)).toBe("severity--info");
+	});
 });
 
 describe('severityText', function() {
@@ -76,6 +82,12 @@ describe('severityText', function() {
 		var severityText;
 		severityText = $filter('severityText');
 		expect(severityText(1)).toBe("OVERVIEW.INFO");
+	});
+
+	it('should return OVERVIEW.INFO', function() {
+		var severityText;
+		severityText = $filter('severityText');
+		expect(severityText(0)).toBe("OVERVIEW.INFO");
 	});
 
 	it('should return OVERVIEW.WARNING', function() {
@@ -130,5 +142,88 @@ describe('healthPanelClass', function() {
 		healthPanelClass = $filter('healthPanelClass');
 		expect(healthPanelClass("--")).toBe("cr-panel--default");
 	});
+
+	it('should return panel-default', function() {
+		var healthPanelClass;
+		healthPanelClass = $filter('healthPanelClass');
+		expect(healthPanelClass("")).toBe("cr-panel--default");
+	});
 });
 
+
+describe('queryStatusClass', function() {
+	'use strict';
+
+	var $filter;
+
+	beforeEach(function() {
+		module('crate');
+		module('common');
+	});
+
+	beforeEach(function() {
+		inject(function(_$filter_) {
+			$filter = _$filter_;
+		});
+	});
+
+	it('should return query-status--ok', function() {
+		var queryStatusClass;
+		queryStatusClass = $filter('queryStatusClass');
+		expect(queryStatusClass("OK")).toBe("query-status--ok");
+	});
+
+	it('should return query-status--error', function() {
+		var queryStatusClass;
+		queryStatusClass = $filter('queryStatusClass');
+		expect(queryStatusClass("ERROR")).toBe("query-status--error");
+	});
+
+	it('should return ..', function() {
+		var queryStatusClass;
+		queryStatusClass = $filter('queryStatusClass');
+		expect(queryStatusClass("something")).toBe("");
+	});
+});
+
+
+describe('languageFilter', function() {
+	'use strict';
+
+	var $filter;
+
+	beforeEach(function() {
+		module('crate');
+		module('common');
+	});
+
+	beforeEach(function() {
+		inject(function(_$filter_) {
+			$filter = _$filter_;
+		});
+	});
+
+	it('should return english', function() {
+		var languageFilter;
+		languageFilter = $filter('languageFilter');
+		expect(languageFilter("en")).toBe("English");
+	});
+
+	it('should return deutsche', function() {
+		var languageFilter;
+		languageFilter = $filter('languageFilter');
+		expect(languageFilter("de")).toBe("Deutsch");
+	});
+
+	it('should return espagnol', function() {
+		var languageFilter;
+		languageFilter = $filter('languageFilter');
+		expect(languageFilter("es")).toBe("Español");
+	});
+
+	it('should return auto', function() {
+		var languageFilter;
+		languageFilter = $filter('languageFilter');
+		expect(languageFilter("something")).toBe("Auto");
+	});
+});
