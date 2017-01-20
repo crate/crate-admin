821b788c29eed998401c7179437b589ad929d47c
meriamk <meriam.kharbat@gmail.com>
2017-01-20 15:47:51 +0100

    update node graphs to angular-nvd3

--- app/scripts/controllers/cluster.js
+++ app/scripts/controllers/cluster.js
@@ -2,7 +2,7 @@
 
 angular.module('cluster', ['stats', 'sql', 'common', 'nodeinfo'])
   .controller('NodeListController', function($scope, $route,
-    ClusterState, prepareNodeList, NodeListInfo, compareByHealth){
+    ClusterState, prepareNodeList, NodeListInfo, compareByHealth) {
 
     $scope.nodes = [];
     $scope.selected = null;
@@ -32,10 +32,10 @@ angular.module('cluster', ['stats', 'sql', 'common', 'nodeinfo'])
           // sort nodes by health and hostname
           nodeList = nodeList.sort(compareByHealth);
           // show sidebar
-          var nodeIds = nodeList.map(function(obj){
+          var nodeIds = nodeList.map(function(obj) {
             return obj.id;
           });
-          if (nodeId && nodeIds.indexOf(nodeId)>=0) {
+          if (nodeId && nodeIds.indexOf(nodeId) >= 0) {
             var selectedNode = nodeList.filter(function(node) {
               return node.id === nodeId;
             });
@@ -75,6 +75,51 @@ angular.module('cluster', ['stats', 'sql', 'common', 'nodeinfo'])
     var byteFormatFunction = $filter('bytes');
 
     $scope.node = null;
+
+    var options = {
+      'chart': {
+        'type': 'multiBarHorizontalChart',
+        'height': 20,
+        'margin': {
+          'left': 0,
+          'top': 0,
+          'bottom': 0,
+          'right': 0
+        },
+        'forceY': [0, 100],
+        'showControls': false,
+        'showValues': false,
+        'duration': 500,
+        'showLegend': false,
+        'stacked': true,
+        'showXaxis': false,
+        'showYaxis': false,
+        'x': function(d) {
+          return d.label;
+        },
+        'y': function(d) {
+          return d.value;
+        },
+        'xAxis': {
+          showMaxMin: false
+        },
+        'yAxis':{}
+
+      }
+    };
+    $scope.percent_options = angular.copy(options);
+    $scope.percent_options.chart.yAxis = {
+      tickFormat: function(d) {
+        return String(d) + '%';
+      }
+    };
+
+    $scope.bytes_options = angular.copy(options);
+    $scope.bytes_options.chart.yAxis = {
+      tickFormat: function(d) {
+        return byteFormatFunction(d, 2);
+      }
+    };
 
     var empty = {
       'name': '',
@@ -129,10 +174,10 @@ angular.module('cluster', ['stats', 'sql', 'common', 'nodeinfo'])
       };
       if (node.fs.data) {
         var dataDisks = [];
-        for (var k=0; k<node.fs.data.length; k++) {
+        for (var k = 0; k < node.fs.data.length; k++) {
           dataDisks.push(node.fs.data[k].dev);
         }
-        for (var j=0; j<node.fs.disks.length; j++) {
+        for (var j = 0; j < node.fs.disks.length; j++) {
           var disk = node.fs.disks[j];
           var isDataDisk = dataDisks.indexOf(disk.dev) > -1;
           if (isDataDisk) {
@@ -148,7 +193,7 @@ angular.module('cluster', ['stats', 'sql', 'common', 'nodeinfo'])
     };
 
     var getShardsCountPerState = function(shardInfo, state) {
-      return shardInfo.filter(function (shard) {
+      return shardInfo.filter(function(shard) {
         return shard.state === state;
       }).reduce(function(acc, table) {
         return acc + table.count;
@@ -157,69 +202,87 @@ angular.module('cluster', ['stats', 'sql', 'common', 'nodeinfo'])
 
     var drawGraph = function(node) {
 
-      $scope.cpuData = [
-        {
-          'key': 'System',
-          'values': [['CPU', node.cpu.system]],
-          'color': COLORS.used
-        },
-        {
-          'key': 'User',
-          'values': [['CPU', node.cpu.user]],
-          'color': '#5d89fe'
-        },
-        {
-          'key': 'Idle',
-          'values': [['CPU', Math.max(0, 100-node.cpu.system-node.cpu.user-node.cpu.stolen)]],
-          'color': COLORS.free
-        },
-        {
-          'key': 'Stolen',
-          'values': [['CPU', node.cpu.stolen]],
-          'color': '#f6bb41'
-        }];
+      $scope.cpuData = [{
+        'key': 'System',
+        'values': [{
+          'label': 'CPU',
+          'value': node.cpu.system
+        }],
+        'color': COLORS.used
+      }, {
+        'key': 'User',
+        'values': [{
+          'label': 'CPU',
+          'value': node.cpu.user
+        }],
+        'color': '#5d89fe'
+      }, {
+        'key': 'Idle',
+        'values': [{
+          'label': 'CPU',
+          'value': Math.max(0, 100 - node.cpu.system - node.cpu.user - node.cpu.stolen)
+        }],
+        'color': COLORS.free
+      }, {
+        'key': 'Stolen',
+        'values': [{
+          'label': 'CPU',
+          'value': node.cpu.stolen
+        }],
+        'color': '#f6bb41'
+      }];
 
-      $scope.heapData = [
-        {
-          'key': 'Used',
-          'values': [['HEAP', node.heap.used]],
-          'color': COLORS.used
-        },
-        {
-          'key': 'Free',
-          'values': [['HEAP', node.heap.max - node.heap.used]],
-          'color': COLORS.free
-        }
-      ];
+      $scope.heapData = [{
+        'key': 'Used',
+        'values': [{
+          'label': 'HEAP',
+          'value': node.heap.used
+        }],
+        'color': COLORS.used
+      }, {
+        'key': 'Free',
+        'values': [{
+          'label': 'HEAP',
+          'value': node.heap.max - node.heap.used
+        }],
+        'color': COLORS.free
+      }];
 
-      $scope.diskUsageData = [
-        {
-          'key': 'Used',
-          'values': [['Disk Usage', node.fs.used]],
-          'color': COLORS.used
-        },
-        {
-          'key': 'Free',
-          'values': [['Disk Usage', node.fs.available]],
-          'color': COLORS.free
-        }
-      ];
+      $scope.diskUsageData = [{
+        'key': 'Used',
+        'values': [{
+          'label': 'Disk Usage',
+          'value': node.fs.used
+        }],
+        'color': COLORS.used
+      }, {
+        'key': 'Free',
+        'values': [{
+          'label': 'Disk Usage',
+          'value': node.fs.available
+        }],
+        'color': COLORS.free
+      }];
 
-      $scope.processCpuData = [
-        {
-          'key': 'Used',
-          'values': [['Process CPU', Math.min(100.0, node.proc_cpu.percent / node.num_cores)]],
-          'color': COLORS.used
-        },
-        {
-          'key': 'Idle',
-          'values': [['Process CPU', Math.max(0, 100.0 - node.proc_cpu.percent / node.num_cores)]],
-          'color': COLORS.free
-        }
-      ];
+      $scope.processCpuData = [{
+        'key': 'Used',
+        'values': [{
+          'label': 'Process CPU',
+          'value': Math.min(100.0, node.proc_cpu.percent / node.num_cores)
+        }],
+        'color': COLORS.used
+      }, {
+        'key': 'Idle',
+
+        'values': [{
+          'label': 'Process CPU',
+          'value': Math.max(100.0, node.proc_cpu.percent / node.num_cores)
+        }],
+        'color': COLORS.free
+      }];
     };
 
-    var render = function(nodeId){
+    var render = function(nodeId) {
       if (currentWatcher) {
         // de-register
         currentWatcher();
@@ -230,7 +293,7 @@ angular.module('cluster', ['stats', 'sql', 'common', 'nodeinfo'])
         var cluster = angular.copy(data.cluster);
         var shards = angular.copy(data.shards);
 
-        for (var i=0; i<cluster.length; i++) {
+        for (var i = 0; i < cluster.length; i++) {
           cluster[i].fs = aggregateDataDiskUtilisation(cluster[i]);
         }
         version = data.version;
@@ -270,7 +333,7 @@ angular.module('cluster', ['stats', 'sql', 'common', 'nodeinfo'])
           }
         }
         if ($scope.node && shards && shards.length) {
-          var shardInfoPerNode = shards.filter(function (shard) {
+          var shardInfoPerNode = shards.filter(function(shard) {
             return shard.node_id === $scope.node.id;
           });
           $scope.shardInfo = {
@@ -284,26 +347,28 @@ angular.module('cluster', ['stats', 'sql', 'common', 'nodeinfo'])
       }, true);
     };
 
-    $scope.toolTipUsedPercentFunction = function(){
+    $scope.toolTipUsedPercentFunction = function() {
       return function(key, x, y) {
-        return '<p>' + key +'<br /><b>' + y + '%</b></p>';
+        return '<p>' + key + '<br /><b>' + y + '%</b></p>';
       };
     };
 
-    $scope.toolTipUsedBytesFunction = function(){
+    $scope.toolTipUsedBytesFunction = function() {
       return function(key, x, y) {
-        return '<p>' + key +'<br /><b>' + y + '</b></p>';
+        return '<p>' + key + '<br /><b>' + y + '</b></p>';
       };
     };
 
-    $scope.yAxisByteFormatFunction = function(){
+    $scope.yAxisByteFormatFunction = function() {
       return function(d) {
         return byteFormatFunction(d, 2);
       };
     };
 
     // bind tooltips
-    $('[rel=tooltip]').tooltip({ placement: 'top'});
+    $('[rel=tooltip]').tooltip({
+      placement: 'top'
+    });
 
     // sidebar button handler (mobile view)
     $scope.toggleSidebar = function() {
@@ -311,7 +376,7 @@ angular.module('cluster', ['stats', 'sql', 'common', 'nodeinfo'])
       $('.menu-toggle i.fa').toggleClass('fa-angle-double-right').toggleClass('fa-angle-double-left');
     };
 
-    $scope.isSameVersion = function(nodeVersion){
+    $scope.isSameVersion = function(nodeVersion) {
       return version ? nodeVersion.build_hash === version.hash : true;
     };
 
