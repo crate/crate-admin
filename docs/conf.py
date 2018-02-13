6b4bdcdbddcdda170f01247662511b43461b7783
Naomi Slater <nslater@tumbolia.org>
2018-02-13 16:35:31 +0100

    fix theme config

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,4 @@
-from crate.theme.rtd.conf.crash import *
+from crate.theme.rtd.conf.admin_ui import *
 
 site_url = 'https://crate.io/docs/clients/admin-ui/en/latest/'
 extensions = ['sphinx_sitemap']
