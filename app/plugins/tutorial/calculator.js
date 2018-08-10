9cfe5f639ff90d685e8f8cbd1d7556e2c2c52495
Heeill(Floyd)Wang <heeillfloydwang@heeillfwangsmbp.speedport_w_723v_1_45_000>
2018-08-10 16:54:13 +0200

    test calculator

---
+++ app/plugins/tutorial/calculator.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// storage is measured in Bytes
+// time is measured in hours
+angular.module('calculator', ['sql', 'translation']).controller('CalculatorController', function($scope) {
+    $scope.test = 'test string';
+});
