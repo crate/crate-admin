e3aa364dbbe5babb29e81f0ad768a8c0970055ce
leo <leonhard@crate.io>
2018-08-27 10:43:28 +0200

    fixup variabe and function scopes

--- app/plugins/tutorial/calculator.js
+++ app/plugins/tutorial/calculator.js
@@ -4,14 +4,14 @@
 // time is measured in hours
 
 angular.module('calculator', ['sql', 'translation']).controller('CalculatorController', function($scope, SQLQuery, queryResultToObjects) {
-    $scope.diskLoadFactor = 0.85;
-    $scope.maxRAMPerNode = 64000000000; //64G
+    var diskLoadFactor = 0.85;
+    var maxRAMPerNode = 64000000000; //64G
     $scope.RAMInput = 64;
     $scope.RAMInputUnitPrefix = 'Giga';
     $scope.hideGCHint = true;
-    $scope.sizeFactor = 0.732; //from haudi's document
-    $scope.maxShardSize = 32000000000; //32G, compromise from haudi's and andrei's opinions
-    $scope.maxShards = 1000;
+    var sizeFactor = 0.732; //from haudi's document
+    var maxShardSize = 32000000000; //32G, compromise from haudi's and andrei's opinions
+    var maxShards = 1000;
     $scope.CPUCoresPerNode = 2;
     $scope.RAMStorageProportion = 24;
     $scope.dataType = 'perTime';
@@ -20,62 +20,61 @@ angular.module('calculator', ['sql', 'translation']).controller('CalculatorContr
     $scope.expectedTableSizeUnitPrefix = 'Terra';
     $scope.dataInsertedPerTimeUnitPrefix = 'Giga';
     $scope.dataInsertedPerTimeTemporalUnit = 'day';
-    $scope.expectedTableSize = 10;
     $scope.keepTimeTemporalUnit = 'month';
     $scope.keepTime = 6;
     $scope.partitionSize = 1;
     $scope.partitionSizeTemporalUnit = 'month';
     $scope.manualPartitionCount = 4;
     $scope.replicas = 1;
-    $scope.tables = ["table1", "table2"];
-    $scope.selectSchema = "none";
-    $scope.selectTable = "none";
+    $scope.tables = [];
+    var selectSchema = "none";
+    var selectTable = "none";
     $scope.selected = "none";
 
     $scope.selectedRAM = function (){
-        var r = $scope.RAMInput * $scope.prefix($scope.RAMInputUnitPrefix);
-        $scope.hideGCHint = r <= $scope.maxRAMPerNode;
+        var r = $scope.RAMInput * prefix($scope.RAMInputUnitPrefix);
+        $scope.hideGCHint = r <= maxRAMPerNode;
         return r;
     };
-    $scope.neededDiskSpace = function () {
+    var neededDiskSpace = function () {
         var res = 1;
         if ($scope.dataType === 'absolute') {
-            res = ($scope.expectedTableSize * $scope.prefix($scope.expectedTableSizeUnitPrefix) * (1 + Number($scope.replicas))) / $scope.sizeFactor / $scope.diskLoadFactor;
+            res = ($scope.expectedTableSize * prefix($scope.expectedTableSizeUnitPrefix) * (1 + Number($scope.replicas))) / sizeFactor / diskLoadFactor;
         } else if ($scope.dataType === 'perTime') {
-            res = (($scope.prefix($scope.dataInsertedPerTimeUnitPrefix) * $scope.dataInsertedPerTime / $scope.temporalUnit($scope.dataInsertedPerTimeTemporalUnit)) * $scope.keepTime * $scope.temporalUnit($scope.keepTimeTemporalUnit) * (1 + Number($scope.replicas))) / $scope.diskLoadFactor / $scope.sizeFactor; //explicit cast of replica to number is necessary, otherwise 1+1=11. thanks java script
+            res = ((prefix($scope.dataInsertedPerTimeUnitPrefix) * $scope.dataInsertedPerTime / temporalUnit($scope.dataInsertedPerTimeTemporalUnit)) * $scope.keepTime * temporalUnit($scope.keepTimeTemporalUnit) * (1 + Number($scope.replicas))) / diskLoadFactor / sizeFactor; //explicit cast of replica to number is necessary, otherwise 1+1=11. thanks java script
         }
         return res;
     };
     $scope.neededNodes = function () {
-        return Math.ceil(($scope.neededDiskSpace() / $scope.RAMStorageProportion) / $scope.selectedRAM());
+        return Math.ceil((neededDiskSpace() / $scope.RAMStorageProportion) / $scope.selectedRAM());
     };
     $scope.partitions = function () {
         var res = 1;
         if ($scope.dataType === 'perTime') {
-            res = (($scope.keepTime * $scope.temporalUnit($scope.keepTimeTemporalUnit)) / ($scope.partitionSize * $scope.temporalUnit($scope.partitionSizeTemporalUnit)));
+            res = (($scope.keepTime * temporalUnit($scope.keepTimeTemporalUnit)) / ($scope.partitionSize * temporalUnit($scope.partitionSizeTemporalUnit)));
         } else if ($scope.dataType === 'absolute') {
             res = $scope.manualPartitionCount;
         }
         return res;
     };
-    $scope.shards = function () {
+    var shards = function () {
         return Math.ceil(($scope.neededNodes() * $scope.CPUCoresPerNode) / $scope.partitions());
     };
-    $scope.shardSize = function (shards) {
-        return $scope.neededDiskSpace() / (shards * $scope.partitions() * (1 + Number($scope.replicas)));
+    var shardSize = function (shards) {
+        return neededDiskSpace() / (shards * $scope.partitions() * (1 + Number($scope.replicas)));
     };
     $scope.ramString = function () {
-        return $scope.bytesToPrintableString($scope.selectedRAM(), 0);
+        return bytesToPrintableString($scope.selectedRAM(), 0);
     };
     $scope.storageString = function () {
         if ($scope.neededNodes()!==0){
-            return $scope.bytesToPrintableString(Math.round(Number($scope.neededDiskSpace()) / Number($scope.neededNodes())), 2);
+            return bytesToPrintableString(Math.round(Number(neededDiskSpace()) / Number($scope.neededNodes())), 2);
         }
         else{
             return 0;
         }
     };
-    $scope.bytesToPrintableString = function (b, decimals) {
+    var bytesToPrintableString = function (b, decimals) {
         var strg;
         if (b < Math.pow(10, 3)) {
             strg = b + "B";
@@ -90,7 +89,7 @@ angular.module('calculator', ['sql', 'translation']).controller('CalculatorContr
         }
         return strg;
     };
-    $scope.prefix = function (x) {
+    var prefix = function (x) {
         switch (x) {
             case "Terra":
                 return Math.pow(10, 12);
@@ -104,7 +103,7 @@ angular.module('calculator', ['sql', 'translation']).controller('CalculatorContr
                 return Math.pow(10, 0);
         }
     };
-    $scope.temporalUnit = function (x) {
+    var temporalUnit = function (x) {
         switch (x) {
             case "hour":
                 return 1;
@@ -121,11 +120,11 @@ angular.module('calculator', ['sql', 'translation']).controller('CalculatorContr
         }
     };
     $scope.result = function () {
-        var s = $scope.shards();
-        if ($scope.shardSize(s) > $scope.maxShardSize) {
-            s = Math.ceil(s * ($scope.shardSize(s) / $scope.maxShardSize));
+        var s = shards();
+        if (shardSize(s) > maxShardSize) {
+            s = Math.ceil(s * (shardSize(s) / maxShardSize));
         }
-        if (s > $scope.maxShards) {
+        if (s > maxShards) {
             return "maximum shard limit exceeded, please talk to an crate engineer about your use-case";
         }
         return s;
@@ -147,16 +146,16 @@ angular.module('calculator', ['sql', 'translation']).controller('CalculatorContr
     };
 
     $scope.tableSelected = function () {
-        $scope.selectSchema = $scope.selected[0];
-        $scope.selectTable = $scope.selected[1];
-        $scope.loadCPUCores($scope.selectSchema, $scope.selectTable);
-        $scope.loadTablesize($scope.selectSchema, $scope.selectTable);
-        $scope.loadPartition($scope.selectSchema, $scope.selectTable);
-        $scope.loadReplica($scope.selectSchema, $scope.selectTable);
-        $scope.loadRAMStoragePropotion();
-        $scope.loadRAM();
+        selectSchema = $scope.selected[0];
+        selectTable = $scope.selected[1];
+        loadCPUCores(selectSchema, selectTable);
+        loadTablesize(selectSchema, selectTable);
+        loadPartition(selectSchema, selectTable);
+        loadReplica(selectSchema, selectTable);
+        loadRAMStoragePropotion();
+        loadRAM();
     };
-    $scope.loadCPUCores = function (schemaName, tableName) {
+    var loadCPUCores = function (schemaName, tableName) {
         var stmt = "SELECT os_info['available_processors']\n" +
             "FROM sys.nodes limit 100;";
         SQLQuery.execute(stmt, {}, false, false, false, false).then(function (query) {
@@ -164,7 +163,7 @@ angular.module('calculator', ['sql', 'translation']).controller('CalculatorContr
         });
     };
 
-    $scope.loadTablesize = function(schemaName, tableName) {
+    var loadTablesize = function(schemaName, tableName) {
         var stmt = "select sum(size) from sys.shards where schema_name = '" + schemaName
             + "'and table_name = '"+tableName+"' and primary=true;";
         SQLQuery.execute(stmt, {}, false, false, false, false).then(function (query) {
@@ -175,13 +174,13 @@ angular.module('calculator', ['sql', 'translation']).controller('CalculatorContr
                 return;
             }
             var size = (query.rows[0])[0];
-            $scope.expectedTableSize = Number($scope.getPrefixedNumber(size));
-            $scope.expectedTableSizeUnitPrefix = $scope.getPrefix(size);
+            $scope.expectedTableSize = Number(getPrefixedNumber(size));
+            $scope.expectedTableSizeUnitPrefix = getPrefix(size);
             $scope.dataType = 'absolute';
         });
     };
 
-    $scope.loadPartition = function(schemaName, tableName) {
+    var loadPartition = function(schemaName, tableName) {
         var stmt = "select   partitioned_by from information_schema.tables where table_schema = '"
             +schemaName+"' and table_name = '"+tableName+"';";
         SQLQuery.execute(stmt, {}, false, false, false, false).then(function (query) {
@@ -198,7 +197,7 @@ angular.module('calculator', ['sql', 'translation']).controller('CalculatorContr
         });
     };
 
-    $scope.loadReplica = function(schemaName, tableName) {
+    var loadReplica = function(schemaName, tableName) {
         var rep = "";
         var stmt = "SELECT number_of_replicas FROM information_schema.tables WHERE table_schema='"
             +schemaName+"' and table_name = '"+tableName+"';";
@@ -222,7 +221,7 @@ angular.module('calculator', ['sql', 'translation']).controller('CalculatorContr
         });
     };
 
-    $scope.loadRAMStoragePropotion = function() {
+    var loadRAMStoragePropotion = function() {
         var stmt = "SELECT fs['total']['available']/(heap['used']+heap['free']) AS RAMStoragePropotion FROM sys.nodes;";
         SQLQuery.execute(stmt, {}, false, false, false, false).then(function (query) {
             var sum = 0;
@@ -234,7 +233,7 @@ angular.module('calculator', ['sql', 'translation']).controller('CalculatorContr
         });
     };
 
-    $scope.getPrefix = function(x){
+    var getPrefix = function(x){
         if (x < Math.pow(10, 3)) {
             return "1";
         } else if (x < Math.pow(10, 6)){
@@ -248,7 +247,7 @@ angular.module('calculator', ['sql', 'translation']).controller('CalculatorContr
         }
     };
 
-    $scope.getPrefixedNumber = function(x){
+    var getPrefixedNumber = function(x){
         if (x < Math.pow(10, 3)) {
             return x;
         } else if (x < Math.pow(10, 6)){
@@ -262,7 +261,7 @@ angular.module('calculator', ['sql', 'translation']).controller('CalculatorContr
         }
     };
 
-    $scope.loadRAM = function () {
+    var loadRAM = function () {
         var stmt = "SELECT (heap['used']+heap['free']) AS total_ram FROM sys.nodes;";
         SQLQuery.execute(stmt, {}, false, false, false, false).then(function (query) {
             var sum = 0;
@@ -270,8 +269,8 @@ angular.module('calculator', ['sql', 'translation']).controller('CalculatorContr
                 sum += query.rows[i];
             }
             var avg = Math.round(sum / query.rows.length);
-            $scope.RAMInputUnitPrefix = $scope.getPrefix(avg);
-            $scope.RAMInput = Number($scope.getPrefixedNumber(avg));
+            $scope.RAMInputUnitPrefix = getPrefix(avg);
+            $scope.RAMInput = Number(getPrefixedNumber(avg));
         });
     };
 });
