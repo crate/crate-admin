661606d1bb139ccac0c26a065106637c7df01f34
Savvas Makalias <savvas@crate.io>
2018-11-05 20:50:13 +0200

    Change the license information (ident) to be taken from the `sys.cluster.licence`

--- app/app.module.js
+++ app/app.module.js
@@ -147,9 +147,11 @@ $.get('static/conf/plugins.json', function (plugins) {
       function (SQLQueryProvider, queryResultToObjectsProvider, $ocLazyLoadProvider,
         $stateProvider, SettingsProvider) {
 
-        var stmt = 'SELECT settings[\'license\'][\'enterprise\'] as enterprise, ' +
-          'settings[\'license\'][\'ident\'] as ident ' +
-          'FROM sys.cluster';
+        var stmt = "SELECT settings['license']['enterprise'] as enterprise, " +
+            "if(license is not NULL, " +
+                "format('%s (exp: %tF)', license['issuedTo'], license['expirationDateInMs']), " +
+                "'') as ident " +
+            "FROM sys.cluster";
 
         if (SettingsProvider.$get().enterprise === true) {
           loadStylesheet('static/styles/enterprise.css');
