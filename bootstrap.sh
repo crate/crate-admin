61bb3c2e35b0bfb00df6dce166afa7f814b3ce73
Andreas Motl <andreas.motl@crate.io>
2024-01-12 06:07:44 +0100

    Sandbox: Update to Node.js 20

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Install Node.js sandbox.
-export NODEJS_VERSION=18.12.1
-export NPM_VERSION=8.19.3
+export NODEJS_VERSION=20.11.0
+export NPM_VERSION=10.3.0
 source /dev/stdin <<<"$(curl -s https://raw.githubusercontent.com/cicerops/supernode/main/supernode)"
 
 # Install dependencies.
