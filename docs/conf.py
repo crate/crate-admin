32b8f982d853dded3b6a93cf64c7225902105225
Matthias <matthias@crate.io>
2024-04-22 14:35:19 +0200

    disable leftover version chooser

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,10 @@
 from crate.theme.rtd.conf.crate_admin_ui import *
 
 html_extra_path = []
+
+# Disable version chooser.
+html_context.update({
+    "display_version": False,
+    "current_version": None,
+    "versions": [],
+})
