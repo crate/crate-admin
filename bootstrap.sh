b42c1022e2ca1f9ce3923303c5869733e810ab33
Andreas Motl <andreas.motl@crate.io>
2024-10-25 14:00:03 +0200

    Sandbox: Update to Node.js 22.10 and NPM 10.9

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Install Node.js sandbox.
-export NODEJS_VERSION=20.11.0
-export NPM_VERSION=10.3.0
+export NODEJS_VERSION=22.10.0
+export NPM_VERSION=10.9.0
 source /dev/stdin <<<"$(curl -s https://raw.githubusercontent.com/cicerops/supernode/main/supernode)"
 
 # Install dependencies.
